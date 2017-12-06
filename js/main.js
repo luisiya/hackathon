16cce88c2141f13034227d537e743b0dfa066ffd
Kravchenko Luidmila <Luisiya@gmail.com>
2017-12-06 20:25:32 +0200

    nav

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
 
         $(document).ready(function() { 
             var windowWidth = $(window).width();
-            if(windowWidth > 768)$("#nav").addClass("navbar-fixed-top");
+            if(windowWidth > 997)$("#nav").addClass("navbar-fixed-top");
             else $("#nav").removeClass("navbar-fixed-top ");
             
             $(window).resize(function(){
                 var windowWidth = $(window).width();
-                if(windowWidth > 768)$("#nav").addClass("navbar-fixed-top");
+                if(windowWidth > 997)$("#nav").addClass("navbar-fixed-top");
                 else $("#nav").removeClass("navbar-fixed-top");
             });
         });
